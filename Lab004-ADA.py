d59c18ed21dc3f71550451663414378f92a38c4f
Mohit 202093 B.Tech Cse <62788802+Mohit-MK@users.noreply.github.com>
2022-11-11 23:10:23 +0530

    Update Lab004-ADA.py

--- Lab004-ADA.py
+++ Lab004-ADA.py
@@ -1,18 +1,22 @@
-def knapsack(W_bag,s_a):
-    s_a=sorted(s_a,key=lambda i:i[1],reverse=True)
-    result=0
-    for item in s_a:
-        if item[0]<W_bag:
-            W_bag=W_bag-item[0]
-            result=result+item[1]
-    return (result)    
+def knapSack(maxCapacity, sortedWeights, sortedValues):
+    max_profit = 0
+    size = len(sortedWeights)
 
+    if size == 0:
+        return max_profit
+    
+    for i in range(0, size):
+        # Exit condition
+        if(maxCapacity == 0):
+            break
 
-W_bag=int(input("Enter the bag size:"))
-wt_item=input("Enter the weight of the items:").split()
-wt_item=[int(item) for item in wt_item]
-val_item=input("Enter the value of the item:").split()
-val_item=[int(item) for item in val_item]
-s_a=[[wt_item[i],val_item[i]] for i in range(3)]
-print (s_a)
-print(knapsack(W_bag,s_a))
+        if(sortedWeights[i] <= maxCapacity):
+            max_profit += sortedValues[i]
+            maxCapacity -= sortedWeights[i]
+
+    return max_profit
+
+val = [120, 100, 60, 80, 40, 20, 5]
+wt = [55, 42, 38, 31, 30, 20, 10]
+maxCapacity = 90
+print (knapSack(maxCapacity, wt, val))
